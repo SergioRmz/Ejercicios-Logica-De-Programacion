477293109cb333c08f6e63ef502113b10483699c
Sergio Ramirez <sergio.ramirez.mtz@gmail.com>
2023-03-30 00:32:22 -0600

    anade el diagrama de flujo

--- Ejercicio de Lógica de Programación 4/ejercicio4.js
+++ Ejercicio de Lógica de Programación 4/ejercicio4.js
@@ -1,25 +1,26 @@
 // Declaración de variable para guardar el input del usuario.
-// let userInput;
+let userInput;
 // Validación de los datos introducidos por el usuario.
-// do {
-//     userInput = parseFloat(prompt("Introduce un numero entero positivo mayor a 0"));
-// } while (isNaN(userInput) || userInput < 1);
+do {
+    userInput = parseInt(prompt("Introduce un numero entero positivo mayor o igual a 0"));
+} while (isNaN(userInput) || userInput < 0);
 
 
 let fibonacci = (userInput) => {
-    if (userInput <= 1) {
-        return 1;
-    } else{
+    if (userInput == 0) return 0;
+    else if (userInput == 1) return 1;
+    else{
         return (fibonacci(userInput - 1) + fibonacci(userInput - 2));
     }
 };
 
-console.log(fibonacci(5 ));
+// Se trae el parrafo con id fibonacci 
+let paragraph = document.querySelector("#fibonacci");
+// Se crea un nodo de texto para mostrar el resultado de la funcion
+let paragraphText = document.createTextNode(fibonacci(userInput));
 
-// let paragraphText = document.createTextNode(fibonacci(userInput));
-// let paragraph = document.querySelector("#fibonacci");
-
-// paragraph.append(paragraphText);
+// Se anade el texto creado al parrafo.
+paragraph.append(paragraphText);
 
 
 
