753aff3bb4057a8f6d8b9bd52697bd5c449132a6
Sergio Ramirez <sergio.ramirez.mtz@gmail.com>
2023-03-30 01:58:19 -0600

    cambia el archivo .java

--- Ejercicio de Lógica de Programación 6/StringReversed.java
+++ Ejercicio de Lógica de Programación 6/StringReversed.java
@@ -5,19 +5,24 @@ import java.util.Scanner;
 public class StringReversed {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Introduce una cadena a invertir: ");
+		
 		Scanner input = new Scanner(System.in);
-		
+		System.out.println("Introduce una cadena a invertir: ");
 		String inputUser = input.nextLine();
-		char [] arrayInput = inputUser.toCharArray();
-		String cadenaInvertida = "";
 		
-		for (int i = arrayInput.length - 1; i >= 0; i--) {
-			cadenaInvertida += arrayInput[i];
+		String cadenaInvertida = "";
+		if (inputUser.matches("\\d+")) {
+			System.out.println("Intenta nuevamente");
+		} else {
+			char [] arrayInput = inputUser.toCharArray();
+			for (int i = arrayInput.length - 1; i >= 0; i--) {
+				cadenaInvertida += arrayInput[i];
+			}
 		}
 		
 		System.out.println(cadenaInvertida);
+		
+		input.close();
 	}
 
 }
